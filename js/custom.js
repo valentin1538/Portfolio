0ae7228fb4515c58f8203574a7e7ee6c7a5e0fe0
Valentin <37328458+valentin1538@users.noreply.github.com>
2023-11-06 10:12:22 +0100

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -17,3 +17,37 @@ $(function()
     new WOW().init();
     $(".rotate").textrotator();
 })
+  
+$
+
+function handleIntersection(entries, observer) {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			// L'élément est devenu visible, exécutez votre code JavaScript ici
+			console.log("L'élément est visible à l'écran.");
+			$(".sql").animate({ 
+				width: "95%", 
+			}, 2600); 
+			$(".web").animate({ 
+				width: "80%", 
+			}, 2500); 
+			$(".algo").animate({ 
+				width: "70%", 
+			}, 2300); 
+			$(".c").animate({ 
+				width: "90%", 
+			}, 2600); 
+			// Une fois que vous avez exécuté votre code, vous pouvez cesser d'observer l'élément.
+			observer.unobserve(entry.target);
+		}
+	});
+}
+
+// Créez un observer qui observe l'élément avec la classe "trigger-element"
+const observer = new IntersectionObserver(handleIntersection);
+
+// Sélectionnez l'élément à observer
+const elementToObserve = document.querySelector('.trigger-element');
+
+// Commencez à observer l'élément
+observer.observe(elementToObserve);
